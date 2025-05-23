3890e7539b86e0af19272941e5760d1968fcef09
SebasGomez4 <s.gomez7@udea.edu.co>
2025-05-23 10:30:11 -0500

    Merge pull request #2 from hotfix/correction-of-responsibilities

--- TiendaLaSebastiana/src/BusinessLogic/Mecato.java
+++ TiendaLaSebastiana/src/BusinessLogic/Mecato.java
@@ -9,64 +9,24 @@ package BusinessLogic;
 
 import java.util.ArrayList;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 /**
  *
  * @author Sebastian
  */
 public class Mecato extends Producto {
-    private LocalDate fechaDeVencimiento;
-    private ArrayList<String> etiquetas;
 
     public Mecato() {
     }
     
     public Mecato(String nombre, long id, double precio, double precioMayorista,
                   LocalDate fechaDeVencimiento, ArrayList<String> etiquetas) {
-        super(nombre,id,precio, precioMayorista, EnumTipoProd.MECATO);
-        this.fechaDeVencimiento = fechaDeVencimiento;
-        this.etiquetas = etiquetas;
-    }
-
-    public LocalDate getFechaDeVencimiento() {
-        return fechaDeVencimiento;
-    }
-
-    public void setFechaDeVencimiento(LocalDate fechaDeVencimiento) {
-        this.fechaDeVencimiento = fechaDeVencimiento;
-    }
-
-    public ArrayList<String> getEtiquetas() {
-        return etiquetas;
-    }
-
-    public void setEtiquetas(ArrayList<String> etiquetas) {
-        this.etiquetas = etiquetas;
+        super(nombre,id,precio, precioMayorista, EnumTipoProd.GRANOS, fechaDeVencimiento, etiquetas);
     }
 
     @Override
     public String imprimirFicha() {
-        StringBuilder ficha = new StringBuilder();
-        ficha.append("=== Ficha Mecato ===\n");
-        ficha.append("Nombre: ").append(getNombre()).append("\n");
-        ficha.append("ID: ").append(getId()).append("\n");
-        ficha.append("Precio Mayor: ").append(getPrecioMayorista()).append("\n");
-        ficha.append("Precio Menor: ").append(getPrecio()).append("\n");
-
-        LocalDate fechaDeVencimiento = getFechaDeVencimiento();
-        if (fechaDeVencimiento != null) {
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd 'de' MMMM 'de' yyyy");
-            ficha.append("Fecha Vencimiento: ").append(fechaDeVencimiento.format(formatter)).append("\n");
-        } else {
-            ficha.append("Fecha Vencimiento: No especificada\n");
-        }
-
-        ficha.append("Etiquetas:\n");
-        for (String etiqueta : getEtiquetas()) {
-            ficha.append("- ").append(etiqueta).append("\n");
-        }
-        return ficha.toString();
+        return Utils.mostrarMenu(this);
     }
 }
 
